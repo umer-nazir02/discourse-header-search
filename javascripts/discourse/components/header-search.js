44f29033763232d6d0ab20d297b5c69857afc387
umer.nazir <117436000+umer-nazir02@users.noreply.github.com>
2025-04-29 18:08:30 +0500

    test-1

--- javascripts/discourse/components/header-search.js
+++ javascripts/discourse/components/header-search.js
@@ -2,12 +2,33 @@ import Component from "@glimmer/component";
 import { service } from "@ember/service";
 import { modifier as modifierFn } from "ember-modifier";
 import { focusSearchInput } from "discourse/components/search-menu";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 export default class HeaderSearch extends Component {
   @service site;
   @service siteSettings;
   @service currentUser;
   @service appEvents;
+  @service router;
+  @tracked activeTab = "Forum"; // Add tracked property for activeTab
+
+  constructor() {
+    super(...arguments);
+    // Subscribe to any activeTab changes
+    this.appEvents.on("activeTab:changed", this.handleActiveTabChange);
+  }
+
+  willDestroy() {
+    super.willDestroy();
+    // Clean up event listener
+    this.appEvents.off("activeTab:changed", this.handleActiveTabChange);
+  }
+
+  @action
+  handleActiveTabChange(tabName) {
+    this.activeTab = tabName;
+  }
 
   handleKeyboardShortcut = modifierFn(() => {
     const cb = () => focusSearchInput();
@@ -26,7 +47,8 @@ export default class HeaderSearch extends Component {
     return (
       this.displayForUser &&
       !this.site.mobileView &&
-      !this.args.outletArgs?.topicInfoVisible
+      !this.args.outletArgs?.topicInfoVisible &&
+      this.activeTab === "Forum" // Only display when activeTab is "Forum"
     );
   }
 }
